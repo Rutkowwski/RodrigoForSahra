9c9a53cf53e72acdd5c1ff2b9e40ae26a6d52faa
Rutkowwski <166821624+Rutkowwski@users.noreply.github.com>
2024-04-16 17:14:35 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-    setTimeout(function() {
-      document.querySelector('.heart').style.display = 'none';
-    }, 5000);  O coração desaparecerá após 5 segundos
-  });
+  setTimeout(function() {
+    document.querySelector('.heart').style.display = 'none';
+  }, 5000); // O coração desaparecerá após 5 segundos
+});
