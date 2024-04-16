834ed054811b61faae7fdb0d573e0d4e045b6eed
Rutkowwski <166821624+Rutkowwski@users.noreply.github.com>
2024-04-16 17:06:22 -0300

    Atualizado

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    setTimeout(function() {
+      document.querySelector('.heart').style.display = 'none';
+    }, 5000);  O coração desaparecerá após 5 segundos
+  });
