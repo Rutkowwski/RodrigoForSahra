22b61debf958d56ef17e3af0c2f4c1d833eb2b47
Rutkowwski <166821624+Rutkowwski@users.noreply.github.com>
2024-05-07 10:05:48 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,12 @@ document.addEventListener('DOMContentLoaded', function() {
     document.querySelector('.heart').style.display = 'none';
   }, 500000); // O coração desaparecerá após 50 segundos
 });
+function playMusic() {
+    var audio = document.getElementById("myAudio");
+    audio.play();
+}
+
+function revealSurprise() {
+    var surpriseDiv = document.getElementById("surpriseMessage");
+    surpriseDiv.style.display = "block";
+}
